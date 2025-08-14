ebc27ae1c0b55b3b4ef67bd8bfd4cef36121a0a5
Callum Begley <cfbegley7@gmail.com>
2025-08-14 20:30:25 +1200

    make games page

---
+++ client/components/GamesPage.tsx
@@ -0,0 +1,9 @@
+const GamesPage = () => {
+   return (
+    <div className="w-screen max-w-screen-xl min-h-screen justify-self-center p-10 drop-shadow-xl/50 scroll-smooth transition-transform duration-1000">
+      <p className="text-4xl font-medium mb-48">Hello</p>
+    </div>
+  )
+}
+
+export default GamesPage
--- client/router.tsx
+++ client/router.tsx
@@ -8,7 +8,7 @@ import App from './components/App'
 import Home from './components/Home'
 import Blog from './components/Blog'
 import Dodgeball from './components/Dodgeball'
-import Games from './components/Games'
+import GamesPage from './components/GamesPage'
 import NotFoundPage from './components/NotFoundPage'
 import Flappy from './components/Flappy'
 import Geo from './components/Geo'
@@ -18,7 +18,7 @@ const routes = createRoutesFromElements(
     <Route index element={<Home />} />
     <Route path="/Blog" element={<Blog />} />
     <Route path="/Dodgeball" element={<Dodgeball />} />
-    <Route path="/Games" element={<Games />} />
+    <Route path="/Games" element={<GamesPage />} />
     <Route path="/Flappy" element={<Flappy />} />
     <Route path="/Geo" element={<Geo />} />
     <Route path="*" element={<NotFoundPage />} />
