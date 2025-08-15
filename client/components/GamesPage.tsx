e358c9f5f4979656a661a9f4a0e831c7075dd220
Callum Begley <cfbegley7@gmail.com>
2025-08-15 22:55:21 +1200

    add dodgeball game page

--- client/components/GamesPage.tsx
+++ client/components/GamesPage.tsx
@@ -25,6 +25,20 @@ const GamesPage = () => {
           <p className="text-lg">A geography guessing game using Google Streetview and Gemini AI API to generate custom locations. Made with React and TypeScript.</p>
         </div>
         </Link>
+        <a href="https://callum-begley.github.io/pundle/pun.html" target="_blank" rel="noopener noreferrer" className="bg-zinc-700 bg-opacity-40 backdrop-blur-sm rounded-2xl place-content-center p-10 ring-2 dark:ring-white 
+        ring-zinc-800 my-10 hover:bg-opacity-60 w-full grid grid-cols-2 gap-10 place-items-center">
+          <img src="/images/pun-game.jpg" alt="Pun Game" className="rounded-lg h-56 w-auto row-span-2"/>
+          <h2 className=" pb-1 text-4xl text-center">Pundle</h2>
+          <p className="text-lg">Like wordle, but funnier. Personal project I made before Dev Academy boot camp. Made with HTML, CSS, and JavaScript.</p>
+        </a>
+        <Link to="/Dodgeball">
+        <div className="bg-zinc-700 bg-opacity-40 backdrop-blur-sm rounded-2xl place-content-center p-10 ring-2 dark:ring-white 
+        ring-zinc-800 my-10 hover:bg-opacity-60 w-full grid grid-cols-2 gap-10 place-items-center">
+          <img src="/images/dodgeball.png" alt="Dodgeball" className="rounded-lg h-56 w-auto row-span-2"/>
+          <h2 className=" pb-1 text-4xl text-center">Dodgeball</h2>
+          <p className="text-lg">A fast-paced arcade game where you dodge falling emojis using physics-based movement. Test your reflexes and see how long you can survive! Built with Matter.js physics engine and React.</p>
+        </div>
+        </Link>
       </div>
     </div>
   )
