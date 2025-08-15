5887bf29eb7e4b24dbbab527638a584111470a2d
Callum Begley <cfbegley7@gmail.com>
2025-08-15 22:34:04 +1200

    add geo game page

--- client/components/GamesPage.tsx
+++ client/components/GamesPage.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router"
+
 const GamesPage = () => {
    return (
     <div className="w-screen max-w-screen-xl min-h-screen justify-self-center p-10">
@@ -15,12 +17,14 @@ const GamesPage = () => {
           <h2 className=" pb-1 text-4xl text-center">Flappy Kiwi</h2>
           <p className="text-lg">A fun and addictive endless runner game with a kiwi twist. First game made in Unity.</p>
         </a>
-        <a href="https://callum-begley.itch.io/insanity" target="_blank" rel="noopener noreferrer" className="bg-zinc-700 bg-opacity-40 backdrop-blur-sm rounded-2xl place-content-center p-10 ring-2 dark:ring-white 
+        <Link to="/Geo">
+        <div className="bg-zinc-700 bg-opacity-40 backdrop-blur-sm rounded-2xl place-content-center p-10 ring-2 dark:ring-white 
         ring-zinc-800 my-10 hover:bg-opacity-60 w-full grid grid-cols-2 gap-10 place-items-center">
-          <img src="https://img.itch.zone/aW1hZ2UvMzc3NzQwNi8yMjQ4MDIxOS5wbmc=/original/%2FHvZj8.png" alt="Insanity Game" className="rounded-lg h-56 w-auto row-span-2"/>
-          <h2 className=" pb-1 text-4xl text-center">Insanity</h2>
-          <p className="text-lg">A chaotic 2d platform puzzle game. Use your clones to help solve the puzzle and escape the levels. Made with unity in 4 days for GMTK 2025 Game Jam with the theme of Loops.</p>
-        </a>
+          <img src="/images/whereOnEarth.png" alt="Where On Earth?" className="rounded-lg h-56 w-auto row-span-2"/>
+          <h2 className=" pb-1 text-4xl text-center">Where On Earth?</h2>
+          <p className="text-lg">A geography guessing game using Google Streetview and Gemini AI API to generate custom locations. Made with React and TypeScript.</p>
+        </div>
+        </Link>
       </div>
     </div>
   )
