1160d82acc1e9412942b91dc913c59519f3954b4
Callum Begley <cfbegley7@gmail.com>
2025-08-15 23:06:11 +1200

    add new pun image game page

--- client/components/GamesPage.tsx
+++ client/components/GamesPage.tsx
@@ -11,12 +11,6 @@ const GamesPage = () => {
           <h2 className=" pb-1 text-4xl text-center">Insanity</h2>
           <p className="text-lg">A chaotic 2d platform puzzle game. Use your clones to help solve the puzzle and escape the levels. Made with unity in 4 days for GMTK 2025 Game Jam with the theme of Loops.</p>
         </a>
-        <a href="https://callum-begley.itch.io/flappy-kiwi" target="_blank" rel="noopener noreferrer" className="bg-zinc-700 bg-opacity-40 backdrop-blur-sm rounded-2xl place-content-center p-10 ring-2 dark:ring-white 
-        ring-zinc-800 my-10 hover:bg-opacity-60 w-full grid grid-cols-2 gap-10 place-items-center">
-          <img src="https://img.itch.zone/aW1nLzIyMDQ3NDg3LnBuZw==/315x250%23c/nDkEdw.png" alt="Flappy Kiwi Game" className="rounded-lg h-56 w-auto row-span-2"/>
-          <h2 className=" pb-1 text-4xl text-center">Flappy Kiwi</h2>
-          <p className="text-lg">A fun and addictive endless runner game with a kiwi twist. First game made in Unity.</p>
-        </a>
         <Link to="/Geo">
         <div className="bg-zinc-700 bg-opacity-40 backdrop-blur-sm rounded-2xl place-content-center p-10 ring-2 dark:ring-white 
         ring-zinc-800 my-10 hover:bg-opacity-60 w-full grid grid-cols-2 gap-10 place-items-center">
@@ -25,13 +19,18 @@ const GamesPage = () => {
           <p className="text-lg">A geography guessing game using Google Streetview and Gemini AI API to generate custom locations. Made with React and TypeScript.</p>
         </div>
         </Link>
+         <a href="https://callum-begley.itch.io/flappy-kiwi" target="_blank" rel="noopener noreferrer" className="bg-zinc-700 bg-opacity-40 backdrop-blur-sm rounded-2xl place-content-center p-10 ring-2 dark:ring-white 
+        ring-zinc-800 my-10 hover:bg-opacity-60 w-full grid grid-cols-2 gap-10 place-items-center">
+          <img src="https://img.itch.zone/aW1nLzIyMDQ3NDg3LnBuZw==/315x250%23c/nDkEdw.png" alt="Flappy Kiwi Game" className="rounded-lg h-56 w-auto row-span-2"/>
+          <h2 className=" pb-1 text-4xl text-center">Flappy Kiwi</h2>
+          <p className="text-lg">A fun and addictive endless runner game with a kiwi twist. First game made in Unity.</p>
+        </a>
         <a href="https://callum-begley.github.io/pundle/pun.html" target="_blank" rel="noopener noreferrer" className="bg-zinc-700 bg-opacity-40 backdrop-blur-sm rounded-2xl place-content-center p-10 ring-2 dark:ring-white 
         ring-zinc-800 my-10 hover:bg-opacity-60 w-full grid grid-cols-2 gap-10 place-items-center">
-          <img src="/images/pun-game.jpg" alt="Pun Game" className="rounded-lg h-56 w-auto row-span-2"/>
+          <img src="/images/pundle2.png" alt="Pun Game" className="rounded-lg h-56 w-auto row-span-2 object-cover"/>
           <h2 className=" pb-1 text-4xl text-center">Pundle</h2>
           <p className="text-lg">Like wordle, but funnier. Personal project I made before Dev Academy boot camp. Made with HTML, CSS, and JavaScript.</p>
         </a>
-        
         <Link to="/Dodgeball">
         <div className="bg-zinc-700 bg-opacity-40 backdrop-blur-sm rounded-2xl place-content-center p-10 ring-2 dark:ring-white 
         ring-zinc-800 my-10 hover:bg-opacity-60 w-full grid grid-cols-2 gap-10 place-items-center">
